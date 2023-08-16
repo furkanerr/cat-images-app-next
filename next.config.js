d9e0f672c990bb72c2dd09cc6bf2c85c64388120
furkaner <furkan08_@hotmail.com>
2023-08-16 20:54:53 +0300

    cache

--- next.config.js
+++ next.config.js
@@ -13,11 +13,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/about',
+        source: '/cats',
         headers: [
           {
             key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
+            value: 's-maxage=1, stale-while-revalidate=2',
           },
         ],
       },
